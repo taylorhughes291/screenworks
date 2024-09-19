4fe444e2c97c762e127fb4f8397159c7faa3d2de
Taylor Hughes <taylor hughes>
2024-09-18 21:03:57 -0700

    uninstalled emotion css and installed emotion react

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
+import {css} from '@emotion/css';
+
 const Header = () => {
     return (
         <header>
             <h1>Screenworks, Inc.</h1>
+            <nav>
+                <ul css={css`
+                    color: red;
+                    text-decoration: none;
+                `}>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Our Story</li>
+                    <li>Photos</li>
+                </ul>
+            </nav>
       </header>
     )
 }
