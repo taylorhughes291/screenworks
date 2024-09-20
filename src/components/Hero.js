c876b12e2c43314c4a427ff9295e196d451cec14
Taylor Hughes <taylor hughes>
2024-09-19 19:19:14 -0700

    completed basic hero call to action

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,12 +4,27 @@ import {css} from '@emotion/react';
 const Hero = ({overrideStyles}) => {
     return (
         <div css={css`
-            background: red;
+            background-image: url('https://picsum.photos/200/200');
+            background-size: contain;
             width: 100%;
             height: 100%;
+            text-align: center;
+            display: flex;
+            justify-content: center;
+            align-items: center;
             ${overrideStyles}
         `}>
-            Hero
+            <div css={css`
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+            `}>
+                <h2>Custom Screenprinting serving Southern California and beyond.</h2>
+                <button css={css`
+                    padding: 10px 20px;
+                    border-radius: 20px;
+                `}>Get a Quote</button>
+            </div>
         </div>
     )
 };
