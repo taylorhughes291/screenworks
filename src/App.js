c6da332b7d6de4671cc1bcac09d8efc1e9b998a1
Taylor Hughes <taylor hughes>
2024-09-19 18:47:28 -0700

    completed basic header layout for mobile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+/** @jsxImportSource @emotion/react */
+import {css} from '@emotion/react';
 import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" css={css`
+      padding: 5px 10px;
+    `}>
       <Header></Header>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,21 @@
 /** @jsxImportSource @emotion/react */
 import {css} from '@emotion/react';
+import burgerMenu from '../assets/burger-menu.svg';
 
 const Header = () => {
     return (
         <header css={css`
             width: 100%;
             display: flex;
-
+            align-items: center;
+            justify-content: space-between;
+            border-bottom: 1px solid black;
         `}>
             <h1>Screenworks, Inc.</h1>
+            <img src={burgerMenu} alt="hamburger menu" css={css`
+                width: 30px;
+                height: 30px;
+            `}/>
             <nav css={css`display: none;`}>
                 <ul css={css`
                     list-style-type: none
