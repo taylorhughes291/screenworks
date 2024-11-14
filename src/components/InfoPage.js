1b2be839ffcfd6a0d61db722b892329a908c2665
Taylor Hughes <taylor hughes>
2024-11-13 19:41:41 -0800

    generalized the info page and added the contact us section

---
+++ src/components/InfoPage.js
@@ -0,0 +1,46 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { colorPalette, commonColors } from "../themes";
+
+const InfoPage = ({title, imgSrc, description, children}) => {
+    return (
+        <div css={css`
+            width: 100%;
+            height: 100vh;
+            background-color: ${colorPalette.color2}
+        `}>
+            <div 
+                css={css`
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    padding: 8px;
+                    border: 2px solid ${colorPalette.color1};
+                    border-radius: 6px;
+                    background-color: ${commonColors.white};
+                    margin: 15px 20px;
+                    box-sizing: border-box;
+                `}
+            >
+                <h2 
+                    css={css`
+                        margin: 0 0 8px;
+                    `}
+                >
+                    {title}
+                </h2>
+                <img src={imgSrc} alt="screenworks founding" />
+                <p 
+                    css={css`
+                        margin: 10px 10px 2px;
+                    `}
+                >
+                    {description}
+                </p>
+                {children}
+            </div>
+        </div>
+    )
+}
+
+export default InfoPage;
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,13 +1,31 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react"
+import InfoPage from "../InfoPage"
 
 const Contact = () => {
     return (
-        <div 
-            css={css`
-                font-weight: 800;
-            `}
-        >Contact</div>
+        <InfoPage title="Contact Us" imgSrc="https://picsum.photos/300/250" description="" >
+            <div 
+                css={css`
+                    width: 100%;
+                    box-sizing: border-box;
+                    padding: 10px 16px;
+                    h3 {
+                        font-size: 16px;
+                    }
+                `}
+            >
+                <h3 
+                    css={css`
+                        margin: 0;
+                    `}
+                >
+                    Phone: (310) 532-7654
+                </h3>
+                <h3>Email: <a href="mailto:screenworkscheri@sbcglobal.net">screenworkscheri@sbcglobal.net</a></h3>
+                <h3>Screenworks, Inc.<br/>5432 135th St<br/>Gardena CA 98765</h3>
+            </div>
+        </InfoPage>
     )
 };
 
