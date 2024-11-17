f77cee9d9dfe50233c58a09bdd99ba38c7b0e169
Taylor Hughes <taylor hughes>
2024-11-17 11:30:20 -0800

    started faq structure

--- src/components/pages/Faq.js
+++ src/components/pages/Faq.js
@@ -1,13 +1,39 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react"
+import InfoPage from "../InfoPage";
 
 const Faq = () => {
+    const faqData = [
+        {
+            question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        },
+        {
+            question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        },
+        {
+            question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        },
+    ];
+
+    const faqRender = faqData.map((item, index) => {
+        return (
+            <div key={index}>
+                <p>{item.question}</p>
+                <p>{item.answer}</p>
+            </div>
+        )
+    });
+
     return (
-        <div
-            css={css`
-                font-weight: 800;
-            `}        
-        >Faq</div>
+        <InfoPage 
+            title="Frequently Asked Questions"
+            imgSrc="https://picsum.photos/300/250"
+        >
+            {faqRender}
+        </InfoPage>
     )
 };
 
