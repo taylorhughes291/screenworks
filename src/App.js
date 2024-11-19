092e7e7986d1f2d6442ef8572ac4f0a8b2af784e
Taylor Hughes <taylor hughes>
2024-11-19 08:56:01 -0800

    successfully retrieved contentful data

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Story from './components/pages/Story';
 import Photos from './components/pages/Photos';
 import Faq from './components/pages/Faq';
 import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import * as contentful from 'contentful'
 
 const AppLayout = () => {
   return (
@@ -28,40 +29,45 @@ const AppLayout = () => {
   )
 };
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <AppLayout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />
-      },
-      {
-        path: "/about",
-        element: <About />
-      },
-      {
-        path: "/contact",
-        element: <Contact />
-      },
-      {
-        path: "/story",
-        element: <Story />
-      },
-      {
-        path: "/photos",
-        element: <Photos />
-      },
-      {
-        path: "/faq",
-        element: <Faq />
-      },
-    ]
-  },
-]);
 
 function App() {
+  let client = contentful.createClient({
+    space: process.env.REACT_APP_CONTENTFUL_SPACE_KEY,
+    accessToken: process.env.REACT_APP_CONTENTFUL_API_TOKEN,
+  });
+  client.getEntries().then((entry) => console.log(entry)).catch((err) => console.log(err))
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/about",
+          element: <About />
+        },
+        {
+          path: "/contact",
+          element: <Contact />
+        },
+        {
+          path: "/story",
+          element: <Story />
+        },
+        {
+          path: "/photos",
+          element: <Photos />
+        },
+        {
+          path: "/faq",
+          element: <Faq />
+        },
+      ]
+    },
+  ]);
   return (
     <RouterProvider router={router} />
   );
