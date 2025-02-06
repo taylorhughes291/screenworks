c04f92524ded4f5eff3778cc70541d892deb2096
Taylor Hughes <taylor hughes>
2025-02-06 08:01:01 -0800

    optimized about page for tablet

--- src/components/InfoPage.js
+++ src/components/InfoPage.js
@@ -1,15 +1,23 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import { colorPalette, commonColors } from "../themes";
+import { colorPalette, commonColors, breakpoints } from "../themes";
 import LoadingWrapper from "./LoadingWrapper";
 
-const InfoPage = ({ title, imgSrc, description, children, isLoaded }) => {
+const InfoPage = ({
+  title,
+  imgSrc,
+  description,
+  children,
+  isLoaded,
+  overrideStyles,
+}) => {
   return (
     <LoadingWrapper isLoaded={isLoaded}>
       <div
         css={css`
           width: 100%;
           background-color: ${colorPalette.color2};
+          ${overrideStyles}
         `}
       >
         <div
@@ -23,37 +31,53 @@ const InfoPage = ({ title, imgSrc, description, children, isLoaded }) => {
             background-color: ${commonColors.white};
             margin: 15px 20px;
             box-sizing: border-box;
+            @media (${breakpoints.tablet}) {
+              padding: 15px 20px;
+            }
           `}
         >
           {title && (
             <h2
               css={css`
                 margin: 0 0 8px;
+                @media (${breakpoints.tablet}) {
+                  margin-bottom: 20px;
+                }
               `}
             >
               {title}
             </h2>
           )}
-          {imgSrc && (
-            <img
-              src={imgSrc}
-              alt="screenworks founding"
-              css={css`
-                max-width: 100%;
-                max-height: 100%;
-              `}
-            />
-          )}
-          {description && (
-            <p
-              css={css`
-                margin: 10px 10px 2px;
-              `}
-            >
-              {description}
-            </p>
-          )}
-          {children}
+          <div
+            className="info-page-content"
+            css={css`
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+            `}
+          >
+            {imgSrc && (
+              <img
+                src={imgSrc}
+                alt="screenworks founding"
+                className="info-page-image"
+                css={css`
+                  max-width: 100%;
+                  max-height: 100%;
+                `}
+              />
+            )}
+            {description && (
+              <p
+                css={css`
+                  margin: 10px 10px 2px;
+                `}
+              >
+                {description}
+              </p>
+            )}
+            {children}
+          </div>
         </div>
       </div>
     </LoadingWrapper>
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,32 +1,59 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import InfoPage from "../InfoPage";
 import { useFetchDatoCms } from "../../helpers/customHooks";
 import { StructuredText } from "react-datocms";
+import { breakpoints } from "../../themes";
 
 const About = () => {
   const query = `query MyQuery {
-  screenworksSite {
-    aboutPageDescription {
-      value
-      blocks
-      links
+    screenworksSite {
+      aboutPageDescription {
+        value
+        blocks
+        links
+      }
+      aboutPageImage {
+        url
+      }
+      aboutPageTitle
     }
-    aboutPageImage {
-      url
-    }
-    aboutPageTitle
-  }
-}`;
+  }`;
 
   const cmsData = useFetchDatoCms(query);
   const pageData = cmsData?.screenworksSite;
+
+  const aboutStyles = css`
+    div.info-page-content {
+      @media (${breakpoints.tablet}) {
+        display: block;
+      }
+    }
+    img.info-page-image {
+      @media (${breakpoints.tablet}) {
+        max-width: 300px;
+        float: left;
+        margin-right: 20px;
+      }
+    }
+  `;
   return (
     <InfoPage
       title={pageData?.aboutPageTitle}
       imgSrc={pageData?.aboutPageImage?.url}
       description=""
       isLoaded={cmsData.isLoaded}
+      overrideStyles={aboutStyles}
     >
-      <StructuredText data={pageData?.aboutPageDescription}></StructuredText>
+      <div
+        css={css`
+          p {
+            margin: 0;
+          }
+        `}
+      >
+        <StructuredText data={pageData?.aboutPageDescription}></StructuredText>
+      </div>
     </InfoPage>
   );
 };
