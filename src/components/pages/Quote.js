87d8865dc68070524ae8608904850b394c9413f0
Taylor Hughes <taylorhughes291@gmail.com>
2025-03-08 08:02:06 -0800

    onload successfully done for quote submit

--- src/components/pages/Quote.js
+++ src/components/pages/Quote.js
@@ -2,6 +2,7 @@
 import { css } from "@emotion/react";
 import { useState, useEffect } from "react";
 import InfoPage from "../InfoPage";
+import LoadingWrapper from "../LoadingWrapper";
 import { colorPalette } from "../../themes";
 import { handleValidations } from "../../helpers/validations";
 import { acceptedFileTypes } from "../../helpers/constants";
@@ -26,6 +27,7 @@ const Quote = () => {
     description: [],
   });
   const [responseStatus, setResponseStatus] = useState(-1);
+  const [requestPending, setRequestPending] = useState(false);
 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
@@ -55,9 +57,12 @@ const Quote = () => {
     });
     if (!totalViolations.length) {
       // Handle form submission logic here
+      setRequestPending(true);
       const eventFormData = new FormData(e.target);
-      const quoteSubmit = await handleQuoteSubmit(eventFormData);
-      setResponseStatus(quoteSubmit);
+      await handleQuoteSubmit(eventFormData).then((res) => {
+        setRequestPending(false);
+        setResponseStatus(res);
+      });
     } else {
       setShowViolations(true);
     }
@@ -84,223 +89,225 @@ const Quote = () => {
   };
 
   return (
-    <InfoPage title="Request a Quote">
-      <form
-        onSubmit={handleSubmit}
-        css={css`
-          display: flex;
-          flex-direction: column;
-          padding: 0 20px 20px;
-          div.quote-field-container {
+    <LoadingWrapper isLoaded={!requestPending}>
+      <InfoPage title="Request a Quote">
+        <form
+          onSubmit={handleSubmit}
+          css={css`
             display: flex;
             flex-direction: column;
-          }
-        `}
-        encType="multipart/form-data"
-      >
-        <div
-          className="quote-field-container"
-          css={css`
-            margin-bottom: 15px;
-          `}
-        >
-          <label
-            css={css`
-              display: block;
-              margin-bottom: 5px;
-              font-weight: bold;
-              color: #555;
-            `}
-          >
-            Name:
-          </label>
-          {renderViolations("name")}
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            css={css`
-              padding: 10px;
-              border: 1px solid #ddd;
-              border-radius: 4px;
-              font-size: 16px;
-            `}
-          />
-        </div>
-        <div
-          className="quote-field-container"
-          css={css`
-            margin-bottom: 15px;
-          `}
-        >
-          <label
-            css={css`
-              display: block;
-              margin-bottom: 5px;
-              font-weight: bold;
-              color: #555;
-            `}
-          >
-            Email:
-          </label>
-          {renderViolations("email")}
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            css={css`
-              padding: 10px;
-              border: 1px solid #ddd;
-              border-radius: 4px;
-              font-size: 16px;
-            `}
-          />
-        </div>
-        <div
-          className="quote-field-container"
-          css={css`
-            margin-bottom: 15px;
-          `}
-        >
-          <label
-            css={css`
-              display: block;
-              margin-bottom: 5px;
-              font-weight: bold;
-              color: #555;
-            `}
-          >
-            How many pieces:
-          </label>
-          {renderViolations("pieces")}
-          <input
-            type="number"
-            name="pieces"
-            value={formData.pieces}
-            onChange={handleChange}
-            css={css`
-              padding: 10px;
-              border: 1px solid #ddd;
-              border-radius: 4px;
-              font-size: 16px;
-            `}
-          />
-        </div>
-        <div
-          className="quote-field-container"
-          css={css`
-            margin-bottom: 15px;
-          `}
-        >
-          <label
-            css={css`
-              display: block;
-              margin-bottom: 5px;
-              font-weight: bold;
-              color: #555;
-            `}
-          >
-            Garments:
-          </label>
-          {renderViolations("garments")}
-          <select
-            name="garments"
-            value={formData.garments}
-            onChange={handleChange}
-            css={css`
-              padding: 10px;
-              border: 1px solid #ddd;
-              border-radius: 4px;
-              font-size: 16px;
-            `}
-          >
-            <option value="">Select a garment</option>
-            <option value="tshirt">T-Shirt</option>
-            <option value="hoodie">Hoodie</option>
-            <option value="jacket">Jacket</option>
-          </select>
-        </div>
-        <div
-          className="quote-field-container"
-          css={css`
-            margin-bottom: 15px;
-          `}
-        >
-          <label
-            css={css`
-              display: block;
-              margin-bottom: 5px;
-              font-weight: bold;
-              color: #555;
-            `}
-          >
-            Art File:
-          </label>
-          {renderViolations("artFile")}
-          <input
-            type="file"
-            name="artFile"
-            multiple
-            onChange={handleChange}
-            css={css`
-              padding: 10px;
-              border: 1px solid #ddd;
-              border-radius: 4px;
-              font-size: 16px;
-            `}
-            accept={inputFileTypes}
-          />
-        </div>
-        <div
-          className="quote-field-container"
-          css={css`
-            margin-bottom: 30px;
-          `}
-        >
-          <label
-            css={css`
-              display: block;
-              margin-bottom: 5px;
-              font-weight: bold;
-              color: #555;
-            `}
-          >
-            Description:
-          </label>
-          {renderViolations("description")}
-          <textarea
-            name="description"
-            value={formData.description}
-            onChange={handleChange}
-            css={css`
-              padding: 10px;
-              border: 1px solid #ddd;
-              border-radius: 4px;
-              font-size: 16px;
-              resize: vertical;
-            `}
-          />
-        </div>
-        <button
-          type="submit"
-          css={css`
-            padding: 10px;
-            background-color: ${colorPalette.color4};
-            border: 1px solid ${colorPalette.color5};
-            border-radius: 4px;
-            font-size: 16px;
-            cursor: pointer;
-            &:hover {
-              background-color: #0056b3;
+            padding: 0 20px 20px;
+            div.quote-field-container {
+              display: flex;
+              flex-direction: column;
             }
           `}
+          encType="multipart/form-data"
         >
-          Submit
-        </button>
-      </form>
-    </InfoPage>
+          <div
+            className="quote-field-container"
+            css={css`
+              margin-bottom: 15px;
+            `}
+          >
+            <label
+              css={css`
+                display: block;
+                margin-bottom: 5px;
+                font-weight: bold;
+                color: #555;
+              `}
+            >
+              Name:
+            </label>
+            {renderViolations("name")}
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              css={css`
+                padding: 10px;
+                border: 1px solid #ddd;
+                border-radius: 4px;
+                font-size: 16px;
+              `}
+            />
+          </div>
+          <div
+            className="quote-field-container"
+            css={css`
+              margin-bottom: 15px;
+            `}
+          >
+            <label
+              css={css`
+                display: block;
+                margin-bottom: 5px;
+                font-weight: bold;
+                color: #555;
+              `}
+            >
+              Email:
+            </label>
+            {renderViolations("email")}
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              css={css`
+                padding: 10px;
+                border: 1px solid #ddd;
+                border-radius: 4px;
+                font-size: 16px;
+              `}
+            />
+          </div>
+          <div
+            className="quote-field-container"
+            css={css`
+              margin-bottom: 15px;
+            `}
+          >
+            <label
+              css={css`
+                display: block;
+                margin-bottom: 5px;
+                font-weight: bold;
+                color: #555;
+              `}
+            >
+              How many pieces:
+            </label>
+            {renderViolations("pieces")}
+            <input
+              type="number"
+              name="pieces"
+              value={formData.pieces}
+              onChange={handleChange}
+              css={css`
+                padding: 10px;
+                border: 1px solid #ddd;
+                border-radius: 4px;
+                font-size: 16px;
+              `}
+            />
+          </div>
+          <div
+            className="quote-field-container"
+            css={css`
+              margin-bottom: 15px;
+            `}
+          >
+            <label
+              css={css`
+                display: block;
+                margin-bottom: 5px;
+                font-weight: bold;
+                color: #555;
+              `}
+            >
+              Garments:
+            </label>
+            {renderViolations("garments")}
+            <select
+              name="garments"
+              value={formData.garments}
+              onChange={handleChange}
+              css={css`
+                padding: 10px;
+                border: 1px solid #ddd;
+                border-radius: 4px;
+                font-size: 16px;
+              `}
+            >
+              <option value="">Select a garment</option>
+              <option value="tshirt">T-Shirt</option>
+              <option value="hoodie">Hoodie</option>
+              <option value="jacket">Jacket</option>
+            </select>
+          </div>
+          <div
+            className="quote-field-container"
+            css={css`
+              margin-bottom: 15px;
+            `}
+          >
+            <label
+              css={css`
+                display: block;
+                margin-bottom: 5px;
+                font-weight: bold;
+                color: #555;
+              `}
+            >
+              Art File:
+            </label>
+            {renderViolations("artFile")}
+            <input
+              type="file"
+              name="artFile"
+              multiple
+              onChange={handleChange}
+              css={css`
+                padding: 10px;
+                border: 1px solid #ddd;
+                border-radius: 4px;
+                font-size: 16px;
+              `}
+              accept={inputFileTypes}
+            />
+          </div>
+          <div
+            className="quote-field-container"
+            css={css`
+              margin-bottom: 30px;
+            `}
+          >
+            <label
+              css={css`
+                display: block;
+                margin-bottom: 5px;
+                font-weight: bold;
+                color: #555;
+              `}
+            >
+              Description:
+            </label>
+            {renderViolations("description")}
+            <textarea
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
+              css={css`
+                padding: 10px;
+                border: 1px solid #ddd;
+                border-radius: 4px;
+                font-size: 16px;
+                resize: vertical;
+              `}
+            />
+          </div>
+          <button
+            type="submit"
+            css={css`
+              padding: 10px;
+              background-color: ${colorPalette.color4};
+              border: 1px solid ${colorPalette.color5};
+              border-radius: 4px;
+              font-size: 16px;
+              cursor: pointer;
+              &:hover {
+                background-color: #0056b3;
+              }
+            `}
+          >
+            Submit
+          </button>
+        </form>
+      </InfoPage>
+    </LoadingWrapper>
   );
 };
 
