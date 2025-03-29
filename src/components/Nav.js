3a9694193d445a8ff66a26a0844443e89aa042c4
Taylor Hughes <taylorhughes291@gmail.com>
2025-03-28 18:33:12 -0700

    fixed header nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -18,14 +18,21 @@ const Nav = ({ overrideStyles = css``, children, onSelect }) => {
       <ul
         css={css`
           list-style-type: none;
+          padding: 0;
           li#${location} {
             font-weight: 800;
           }
           a {
             text-decoration: none;
             color: inherit;
+            white-space: nowrap; /* Prevent text wrapping */
           }
           @media (${breakpoints.desktop}) {
+            li a {
+              font-size: 20px;
+            }
+          }
+          @media (${breakpoints.desktopLarge}) {
             li a {
               font-size: 22px;
             }
